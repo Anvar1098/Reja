a79cd1b8bac1674bb8140bf5999ff6195b5bcab6
Anvar1098 <anvar00aminov@gmail.com>
2024-11-11 20:47:56 +0900

    Updated server.js and harid.js

--- server.js
+++ server.js
@@ -18,13 +18,14 @@ app.set('view engine', 'ejs');
 
 
 //4 Routing code
-app.get('/hello', function (req, res) {
-   res.end('<h1>HELLO WORLD by Jack</h1>');
+app.post("/create-item", (req, res) => {
+   console.log(req.body);
+   res.json({test: "success" });
 });
 
-app.get('/gift', function (req, res) {
-    res.end('<h1>You are in gift page</h1>');
- });
+app.get("/", function (req, res) {
+  res.render("harid");
+});
 
 const server = http.createServer(app);
 let PORT = 3000;
