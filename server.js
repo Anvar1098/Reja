26b5b8a4a6c3a35b8906a4b57cc97c7774c44d69
Anvar1098 <anvar00aminov@gmail.com>
2024-11-14 20:51:55 +0900

    feat: develop author ejs route

--- server.js
+++ server.js
@@ -1,26 +1,40 @@
-console.log('Web Serverni boshlash');
-const express = require('express');
-const app = express();
-const http = require('http');
+console.log('Web Serverni boshlash');  //shunchaki print qildik
+const express = require('express'); //web server yaratish uchun require qildk
+const app = express(); //Initializes an Express application.
+const http = require('http'); // Imports the built-in HTTP module to work with HTTP servers.
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if(err) {
+    console.log("ERROR:", err);
+  }else {
+    user = JSON.parse(data)
+  }
+});
 
 //1 Kirish code
-app.use(express.static('public'));
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.static('public')); // public fayl uchun dostup
+app.use(express.json()); // json formatdagi datani arrayga perevod qiladi
+app.use(express.urlencoded({extended: true})); //Extracts form data from POST requests and makes it accessible in req.body.
 
 
 //2 Session code
 
 
 //3 Views code
-app.set('views', 'views');
-app.set('view engine', 'ejs');
+app.set('views', 'views'); //Specifies the folder named 'views' for template files.
+app.set('view engine', 'ejs'); // Sets 'ejs' as the template engine for rendering views.
 
 
 //4 Routing code
 app.post("/create-item", (req, res) => {
    console.log(req.body);
    res.json({test: "success" });
+});
+
+app.get('/author', (req, res) => {
+    res.render('author', {user: user});
 });
 
 app.get("/", function (req, res) {
