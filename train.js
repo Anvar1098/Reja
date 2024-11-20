a9094f4ec115d89172c2905574ad6a62455b1689
Anvar1098 <anvar00aminov@gmail.com>
2024-11-20 17:02:59 +0900

    fix: added 21and 22 classes

--- train.js
+++ train.js
@@ -34,3 +34,89 @@ function countLetter(letter, word) {
   const result1 = countLetter1("e", "engineer");
   console.log(result1); 
   // bu yechimda esa if/else condition va slice biriktirilgan holatda so'z ichidagi qiymatlarni index boyicha qidirib qiymat olinadi!
+
+//-----------------------------------------------------------------------------------------------------------------
+
+  // 21 (NodeJS eventloop and Callback function)
+console.log('Jack Ma maslahatlari');
+const list = [
+  'yahshi talaba boling',
+  'togri boshliq tanlang va koproq xato qiling',
+  'uzingizga ishlashni boshlang',
+  'siz kuchli bolgan narsani qiling',
+  'endi dam oling, foydasi yuk',
+];
+
+function maslahatBering(a, callback) {
+  if (typeof a !=='number') callback('insert a number', null);
+  else if (a <= 20) callback(null, list[0]);
+  else if (a > 20 && a <=30) callback(null, list[1]);
+  else if (a > 30 && a <=40) callback(null, list[2]);
+  else if (a > 40 && a <=50) callback(null, list[3]);
+  else if (a > 50 && a <=60) callback(null, list[4]);
+  else {
+    callback(null, list[5])
+    /*
+    setTimeout(function () {
+      callback(null, list[5]);
+    }, 5000);
+    */
+  }
+}
+
+maslahatBering(10, (err, data) => {
+ if (err) console.log('ERROR:', err);
+ console.log('javob:', data);
+});
+
+/*
+console.log('passed here 0');
+maslahatBering(65, (err, data) => {
+if (err) console.log('ERROR:', err);
+else {
+  console.log("javob:", data);
+}
+});
+console.log('passed here 1');
+*/
+
+//------------------------------------------------------------------------------------------------------------
+
+// 22 (Asynchronus function)
+
+async function maslahatBering(a)  {
+  if (typeof a !=='number') throw new Error('insert a number');
+  else if (a <= 20) return list[0];
+  else if (a > 20 && a <= 30) return list [1];
+  else if (a > 30 && a <= 40) return list [2];
+  else if (a > 40 && a <= 50) return list [3];
+  else if (a > 50 && a <= 60) return list [4];
+  else {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(list[5]);
+      }, 5000);
+    });
+  }
+}
+
+// console.log('passed here 0');
+// maslahatBering(20)
+//  .then((data) => {
+//    console.log("javob:", data);
+//  })
+//  .catch((err) => {
+//    console.log('ERROR:', err);
+//  });
+//  console.log('passed here 1');
+
+//asyn/ await
+async function run() {
+  let javob = await maslahatBering(20);
+  console.log(javob);
+  javob = await maslahatBering(31);
+  console.log(javob);
+  javob = await maslahatBering(41);
+  console.log(javob);
+}
+run();
